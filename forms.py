501bc35997386973bd9390d0a77c8858e318033d
Hassaan <Hassaan@mail.com>
2025-01-29 09:34:20 +0300

    Added phone number validation to the form

--- forms.py
+++ forms.py
@@ -1,13 +1,14 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SelectField, SubmitField
-from wtforms.validators import DataRequired, Length
+from wtforms.validators import DataRequired, Length, Regexp
 
 class ContactForm(FlaskForm):
     name = StringField('Name')
     phone = StringField('Phone',
                         validators=[
                             DataRequired(),
-                            Length(min=8, max=8, message="Invalid phone number, please enter only 8 digits!")
+                            Length(min=8, max=8, message="Invalid phone number, please enter only 8 digits!"),
+                            Regexp(r'^\d{8}$', message="Phone number must contain only digits")
                             ]
                         )
 
